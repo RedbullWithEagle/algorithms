43e2d55c952f95eca9a142054d495c0c1a5ee6b1
liuchangzhou <liuchangzhou@douyu.tv>
2022-08-22 13:47:46 +0800

    add sort

--- sort/sort_test.go
+++ sort/sort_test.go
@@ -37,3 +37,22 @@ func TestSortArray(t *testing.T) {
 		t.Logf("soucce is %v,is ok", tt.source)
 	}
 }
+
+func sliceEq(a, b []int) bool {
+	// If one is nil, the other must also be nil.
+	if (a == nil) != (b == nil) {
+		return false;
+	}
+
+	if len(a) != len(b) {
+		return false
+	}
+
+	for i := range a {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+
+	return true
+}
